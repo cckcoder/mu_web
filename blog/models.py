2407fa9583ee5017169f7ee7cd27737816e42af0
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 11:38:17 +0700

    add return str

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     body = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title 
