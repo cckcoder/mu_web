17b1d9f60e93562dab0522f8742a8d41c5b63bb4
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 13:16:30 +0700

    add: requirement and use template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hello HOME</h1>")
+    # return HttpResponse("<h1>Hello HOME</h1>")
+    return render(request, 'blog/home.html')
