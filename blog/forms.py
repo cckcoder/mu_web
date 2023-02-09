b6e91187a2f015006bd17dac021551edc76f20f5
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 21:59:56 +0700

    add: contact form

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Contact
+
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        field = ["subject", "sender", "email", "detail"]
