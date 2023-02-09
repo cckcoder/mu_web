b92781213aca69d4c44b99401dc3b1d145d67d3f
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 11:36:17 +0700

    add: auto now on date created and updated

---
+++ README.md
@@ -0,0 +1,18 @@
+# MU Django
+
+## คำสั่งที่ใช้ใน Django
+
+### Setup virtual env
+
+mac os
+> python3 -m venv env
+
+windows os
+> python -m venv env
+
+### Activate env
+
+## Ref Tools
+* [vs code](https://code.visualstudio.com/)
+* [DB Browser](https://sqlitebrowser.org/)
+* [Django web](https://www.djangoproject.com/)
--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     body = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
