d7a3893d9fc2fb2fbd0c6dc688bcaaab69e275b2
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 17:39:28 +0700

    updat: post detail template

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,5 @@ def about(request):
     return render(request, 'blog/about.html')
 
 def post_details(request, post_id):
-    return HttpResponse(f"Post id: {post_id}")
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
