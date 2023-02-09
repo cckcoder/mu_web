11f1eb87c3f747e100e2a28dd794914b3f6649dc
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 10:39:01 +0700

    update: create post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    body = models.TextField()
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
