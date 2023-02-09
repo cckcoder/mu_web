55a796c7b2b15c4594303e852b391b9cdfb51aad
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 13:28:02 +0700

    add: about page

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     # return HttpResponse("<h1>Hello HOME</h1>")
     return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
