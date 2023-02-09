344ea82508b0a91a4f80c175361796d6d17fb1a2
codewizz <c.chaiwat.ch@gmail.com>
2023-02-09 11:06:52 +0700

    add: auto now to date created and updated

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     body = models.TextField()
-    date_created = models.DateTimeField()
-    date_updated = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
